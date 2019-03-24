c426a2790f4ee25fe5205a7728476514f44e6110
Murali <vmurali100@gmail.com>
2019-03-24 15:39:57 +0530

    Componet Commnunication

---
+++ app-basics/src/app/user.ts
@@ -0,0 +1,9 @@
+export interface User{
+    "fname": string; 
+    "lname": string; 
+    "tel": string; 
+    "address": string; 
+    "city": string; 
+    "state": string; 
+    "zip": number; 
+}
